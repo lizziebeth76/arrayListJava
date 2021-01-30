7dc13a540b4a72a7267fec6a7b526e54618977a1
Lizziebeth76 <lizziebeth76@gmail.com>
2021-01-29 20:25:16 -0600

    "fourth commit"

--- src/com/tts/Main.java
+++ src/com/tts/Main.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class Main {
 //THIS PROJECT HAS A VERY ODD TITLE/ FILE PATH STRUCTURE
 //    I DON'T KNOW WHAT HAPPENED OR HOW IT DID. PLEASE FEEL FREE TO HELP IF YOU
-//    ARE READING THIS AND HAVE INSIGHTS!!!!
+//    ARE READING THIS AND HAVE INSIGHTS!!!! I MIGHT HAVE STARTED THE PROJECT AND NAMED IT IN VS CODE
+//    BUT SAVED IT AS UNTITLED HERE (?)
 
 //    YOU WILL FIND WHAT HAVE BECOME SOME VERY INTENTIONALLY DRAMATIC COMMENTS.
 //    I AM TRYING LIKE HECKFIRE TO MAINTAIN A SENSE A SENSE OF HUMOR ABOUT THE FACT
@@ -16,15 +17,9 @@ public class Main {
     public static void main(String[] args) {
 
 //        CREATE PROGRAM THAT FINDS THE FOLLOWING FOR SOME USER INPUT AND STORE IT IN AN ARRAY LIST:
-//        int sum = 0;
-//        int max = 0;
-//        int min = 0;
-//        int product = 1;
-////        int num = 0;
-//        Above is the way jake did it, however, i'm just going to declare each one within its own function
+//        sum; product; largest, smallest of the integers in the user-input array
 
-//
-//
+
 //      CREATES A WELCOME MESSAGE WITHIN THE MAIN METHOD:
         System.out.println("Hey there, friend!");
 
@@ -36,8 +31,7 @@ public class Main {
         ArrayList<Integer> randomNumbers = new ArrayList<>();
 
         System.out.println("Please enter a random number: ");
-        //        Integer someNum = Integer.parseInt(scanner.nextLine()); ---from jake:  to adjust25 (right side of equation) and its pals
-//
+
         int intOne = scanner.nextInt();
         System.out.println("Please enter another random number: ");
         int intTwo = scanner.nextInt();
@@ -54,58 +48,25 @@ public class Main {
         randomNumbers.add(intThree);
         randomNumbers.add(intFour);
         randomNumbers.add(intFive);
-//        calls the function in the main method
+
+//        calls the function in the main method:
         getSum(randomNumbers);
         getProduct(randomNumbers);
         getMax(randomNumbers);
         getMin(randomNumbers);
 
-//        I BELIEVE THAT WHEN I INPUT THE CALLS BELOW, IT WAS A DUPLICATE EFFORT/ ATTEMPT TO CALL THE FUNCTIONS FROM THE MAIN METHOD
-//        I'M PRETTY SURE (IT WOULD APPEAR!) THAT I DON'T ENTIRELY UNDERSTAND WHAT I MEAN WHEN I SAY "CALL THE FUNCTION FROM THE MAIN METHOD
-//        EVEN THO I THINK IT OUGHT TO BE A LOT LIKE IT SOUNDS -- HAALLLLPPPPPP MEEEE, GOOGLE HAS FAILED ME AND I AM TIRED - do NOT DO THIS:
 
-//        randomNumbers.stream().mapToDouble(intOne);
-//        randomNumbers.stream().mapToDouble(intOne);
-//        randomNumbers.stream().mapToDouble(intOne);
-//        randomNumbers.stream().mapToDouble(intOne);
-//        randomNumbers.stream().mapToDouble(intOne);
-//
-//        randomNumbers.product(intOne);
-//        randomNumbers.product(intTwo);
-//        randomNumbers.product(intThree);
-//        randomNumbers.product(intFour);
-//        randomNumbers.product(intFive);
-//
-////        randomNumbers.max(intOne);
-////        randomNumbers.max(intTwo);
-////        randomNumbers.max(intThree);
-////        randomNumbers.max(intFour);
-////        randomNumbers.max(intFive);
-//
-//        randomNumbers.stream().max(intOne);
-//        randomNumbers.stream().max(intTwo);
-//        randomNumbers.stream().max(intThree);
-//        randomNumbers.stream().max(intFour);
-//        randomNumbers.stream().max(intFive);
-//
-//        randomNumbers.stream().min(intOne);
-//        randomNumbers.stream().min(intTwo);
-//        randomNumbers.stream().min(intThree);
-//        randomNumbers.stream().min(intFour);
-//        randomNumbers.stream().min(intFive);
     }
 
-    //    NO ERRORS BELOW (at one point that was true, now that is not), BUT NONE OF THE MATH CURRENTLY WORKS - ONLY THE DEFAULT VALUES DISPLAY
-
-        public static void getSum(ArrayList<Integer> randomNumbers) {
+    public static void getSum(ArrayList<Integer> randomNumbers) {
 // .size is for array lists, and .length is for array
-            int sum = 0;
+        int sum = 0;
         System.out.println("What the heck??" + randomNumbers.toString());
-            for (Integer randomNumber : randomNumbers) {
-                sum += randomNumber;
-            }
-            System.out.println("This is the sum of the numbers you typed in: " + sum);
+        for (Integer randomNumber : randomNumbers) {
+            sum += randomNumber;
         }
+        System.out.println("This is the sum of the numbers you typed in: " + sum);
+    }
 
     public static void getProduct(ArrayList<Integer> randomNumbers) {
 // .size is for array lists, and .length is for array
@@ -117,50 +78,27 @@ public class Main {
         System.out.println("This is the product of the numbers you typed in: " + product);
     }
 
-
-//BELOW YOU WILL SEE A SPOT WHERE THE PROGRAM IS VERY UPSET WITH ME.
-//WE HAVE BEEN DISCUSSING POSSIBLE OPTIONS FOR A SOLUTION THAT MEETS BOTH
-//OF OUR NEEDS, AND WE JUST CANNOT SEEM TO COME TO ANY AGREEMENT.
-//CLEARLY I PLAY A ROLE IN THIS, BUT I REALLY FEEL THE PROGRAM IS BEING LESS THAN
-//FLEXIBLE AND IS NOT WILLING TO MEET ME EVEN HALFWAY. LIKE SERIOUSLY, PROGRAM,
-//I SPENT TWO HOURS ON JUST THIS ONE ISSUE, AND YOU WERE NOT WILLING TO
-//ENTERTAIN EVEN ONE OF THE POTENTIAL SOLUTIONS I OFFERED. RUDE. THIS IS NOT
-//HOW COMPROMISE WORKS IN A RELATIONSHIP, AND I JUST MIGHT HAVE TO OFFICIALLY PUT THE D-WORD
-//ON THE TABLE AT THIS POINT. I HAVE TOO MUCH RESPECT FOR YOU, AND ALSO FOR MYSELF
-//TO CONTINUE BATTLING YOU THIS WAY. FOR NOW, LET JUST TAKE A BREAK, THOUGH...
-
-    //    public static void getLargest(ArrayList<Integer> randomNumbers) { <--THIS WAS WRONG. INTELLIJ BUILT ME THIS:
-
     private static void getMax(ArrayList<Integer> randomNumbers) {
         int max = 0;
+
         for (Integer randomNumber : randomNumbers) {
-//            .randomNumber() = max; --jake had me comment this out
             if (randomNumber > max) max = randomNumber;
         }
         System.out.println("This is the LARGEST of the numbers you typed in: " + max);
     }
+
     private static void getMin(ArrayList<Integer> randomNumbers) {
         int min = 0;
-        if(!randomNumbers.isEmpty()) {
+
+        if (!randomNumbers.isEmpty()) {
             min = randomNumbers.get(0);
         }
         for (Integer randomNumber : randomNumbers) {
-//            .randomNumber() = min; -- jake had me comment this out
             if (randomNumber < min) min = randomNumber;
         }
         System.out.println("This is the smol'est of the numbers you typed in: " + min);
     }
-//    private static void getMin(ArrayList<Integer> randomNumbers) {
-//
-//        int min = 0;
-//
-//        for (Integer randomNumber : randomNumbers) {
-//            min < randomNumber;
-//
-//        }
-//        System.out.println("This is the smol'est of the numbers you typed in: " + min);
 }
-
 
 //SOME OTHER ARRAY LIST METHODS (FROM THE SLIDES):
 //adds an element to a specific index
